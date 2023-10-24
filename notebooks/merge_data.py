541de3cf6cdbdec32472bba54f27d0916559bb6b
AlexFidalgo <alexfidalgo10@gmail.com>
2023-10-24 09:17:19 -0300

    fixes csv appendage with open and clears directory

--- notebooks/merge_data.py
+++ notebooks/merge_data.py
@@ -8,23 +8,36 @@ from transform_data import read_csv
 
 def merge_files(csv_files_dir, output_file):
    
-    merged_data = None
+    separator = ';'
 
-    for file in tqdm(os.listdir(csv_files_dir)):
-        if file.endswith(".csv"):
-            file_path = os.path.join(csv_files_dir, file)
+    with open(output_file, mode="a") as f:
 
-            df = read_csv(file_path)
+        for file in tqdm(os.listdir(csv_files_dir)):
+            if file.endswith(".csv"):
+                file_path = os.path.join(csv_files_dir, file)
 
-            if merged_data is None:
-                merged_data = df
-            else:
-                merged_data = pl.concat([merged_data, df])
+                df = read_csv(file_path)
 
-    merged_data.write_csv(output_file, separator = ';')
+                df.write_csv(f, separator=separator)
+
+def clear(csv_files_dir):
+
+    files = os.listdir(csv_files_dir)
+
+    for file in files:
+        file_path = os.path.join(csv_files_dir, file)
+
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            print(f"Deleted: {file_path}")
 
 if __name__ == '__main__':
     csv_files_dir = './data/csv_files'
-    output_file = './data/csv_files/merged_data.csv'
+    output_file = './data/merged_data.csv'
+    zip_files_dir = './data/zip_files'
 
     merge_files(csv_files_dir, output_file)
+
+    clear(csv_files_dir)
+
+    clear(zip_files_dir)
