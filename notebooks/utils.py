f7191ef436ef5021c82230de082849a89580c987
Alex <alexfidalgo10@gmail.com>
2023-11-13 11:23:33 -0300

    adds read_csv function

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -20,3 +20,29 @@ def replace_values(df, column_name, old_value, new_value):
                             .alias(column_name))
     
     return df
+
+def read_csv(file_path, separator = ','):
+    """
+    Read a CSV file into a Polars DataFrame with specified data types.
+
+    Parameters:
+        file_path (str): The path to the CSV file to be read.
+        separator (str, optional): The delimiter used in the CSV file (default is ',').
+        
+    Returns:
+        pl.DataFrame: A Polars DataFrame containing the data from the CSV file with specified data types.
+    """
+
+    dtypes = {
+        'NumAto': float,
+        'AnguloMeiaPotenciaAntena': float,
+        'PotenciaTransmissorWatts': float,
+        'AlturaAntena': float,
+        'GanhoAntena': str,
+        'AnguloElevacao': float,
+        'Azimute':float,
+        'FrenteCostaAntena': float
+        }
+
+    df = pl.read_csv(file_path, ignore_errors = True, dtypes = dtypes, separator = separator)
+    return df
