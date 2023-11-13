c201aaf52883bdb1f47c963787a53be01d5be516
AlexFidalgo <alexfidalgo10@gmail.com>
2023-11-12 22:41:10 -0300

    creates data and csv_files folders

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,20 +1,25 @@
 import subprocess
+import os
 from extract_data import *
 from merge_data import *
 from transform_data import *
 
-
 if __name__ == '__main__':
+    
+    data_dir = './data'
+    zip_files_dir = os.path.join(data_dir, 'zip_files')
+    csv_files_dir = os.path.join(data_dir, 'csv_files')
+
+    # Create 'data' and 'csv_files' directories if they don't exist
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    if not os.path.exists(csv_files_dir):
+        os.makedirs(csv_files_dir)
 
     script_path = './src/download.ps1'
-    # with open(script_path, 'r') as script_file:
-    #     script_content = script_file.read()
-    #     print(script_content)
     subprocess.run(['powershell', script_path])
 
-    zip_files_dir = './data/zip_files' 
-    csv_files_dir = './data/csv_files'
     unzip_files(zip_files_dir, csv_files_dir)
 
-    zip_files_dir = './data/zip_files'
+    # Optionally, clear 'zip_files' directory after unzipping
     clear(zip_files_dir)
