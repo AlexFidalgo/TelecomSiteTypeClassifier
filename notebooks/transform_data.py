ff1d5c06d0f48e3e839ebc92605f6f598066f623
Alex <alexfidalgo10@gmail.com>
2023-11-14 13:12:24 -0300

    removes whitespace from ClassInfraFisica

--- notebooks/transform_data.py
+++ notebooks/transform_data.py
@@ -7,3 +7,13 @@ csv_files_dir = '../data/csv_files'
 file = 'AC.csv'
 file_path = os.path.join(csv_files_dir, file)
 df = read_csv(file_path, separator = ',')
+
+# removing whitespaces from edges of ClassInfraFisica
+df = (
+    df.select(
+        pl.col('ClassInfraFisica')
+        .map_elements(lambda x: x.strip() if isinstance(x, str) else x)
+        .alias('ClassInfraFisica')
+    )
+)
+
