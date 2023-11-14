cee944717f63667e3105706a49688e14c88a0b8d
Alex <alexfidalgo10@gmail.com>
2023-11-14 13:12:03 -0300

    correctly formulates test function

--- notebooks/test_utils.py
+++ notebooks/test_utils.py
@@ -1,34 +1,31 @@
+import unittest
 import polars as pl
-from utils import strip_column
+import utils
 
-def test_strip_column():
+class TestStripColumn(unittest.TestCase):
 
-    data = {
-        'col1': ['   apple  ', 'banana  ', '   cherry', None, 123],
-        'col2': ['   dog', 'elephant', '   tiger  ', None, 'lion   '],
-        'col3': [1, 2, 3, 4, 5]
-    }
-    df = pl.DataFrame(data)
+    def test_strip_columns(self):
 
-    cleaned_df_col1 = strip_column(df, 'col1')
-    expected_data_col1 = {'col1': ['apple', 'banana', 'cherry', None, 123]}
-    expected_df_col1 = pl.DataFrame(expected_data_col1)
-    assert cleaned_df_col1 == expected_df_col1
+        data = {
+            'col1': ['   apple  ', 'banana  ', '   cherry', None, 123],
+            'col2': ['   dog', 'elephant', '   tiger  ', None, 'lion   '],
+            'col3': [1, 2, 3, 4, 5]
+        }
+        df = pl.DataFrame(data)
 
-    # Test stripping whitespace from 'col2'
-    cleaned_df_col2 = strip_column(df, 'col2')
-    expected_data_col2 = {'col2': ['dog', 'elephant', 'tiger', None, 'lion']}
-    expected_df_col2 = pl.DataFrame(expected_data_col2)
-    assert cleaned_df_col2 == expected_df_col2
+        df = utils.strip_column(df, 'col1')
 
-    # Test stripping whitespace from 'col3'
-    cleaned_df_col3 = strip_column(df, 'col3')  # Should not modify non-string column
-    assert cleaned_df_col3 == df
+        expected_df = pl.DataFrame(
+            data = {
+                'col1': ['apple', 'banana', 'cherry', None, 123],
+                'col2': ['   dog', 'elephant', '   tiger  ', None, 'lion   '],
+                'col3': [1, 2, 3, 4, 5]
+            }
+        )
 
-    # Test stripping whitespace from all columns
-    cleaned_df_all = strip_column(df, 'col1')
-    cleaned_df_all = strip_column(cleaned_df_all, 'col2')
-    cleaned_df_all = strip_column(cleaned_df_all, 'col3')  # Should not modify non-string column
-    assert cleaned_df_all == pl.DataFrame({'col1': ['apple', 'banana', 'cherry', None, 123],
-                                            'col2': ['dog', 'elephant', 'tiger', None, 'lion'],
-                                            'col3': [1, 2, 3, 4, 5]})
+        df_arrow = df.to_arrow()
+        expected_arrow = expected_df.to_arrow()
+        self.assertTrue(df_arrow.equals(expected_arrow))
+
+if __name__ == "__main__":
+    unittest.main()
