53beffa78769405eda7897ebd20a465f0bb7f4cc
Alex <alexfidalgo10@gmail.com>
2023-11-14 15:46:59 -0300

    tests concatenate

--- notebooks/test_utils.py
+++ notebooks/test_utils.py
@@ -1,6 +1,7 @@
 import unittest
 import polars as pl
 import utils
+import pandas as pd
 
 class TestStripColumn(unittest.TestCase):
 
@@ -26,6 +27,27 @@ class TestStripColumn(unittest.TestCase):
         df_arrow = df.to_arrow()
         expected_arrow = expected_df.to_arrow()
         self.assertTrue(df_arrow.equals(expected_arrow))
+
+    def test_concatenate_columns(self):
+        data = {
+            'Tecnologia': ['apple', 'banana', 'cherry', None, '123', 'ab', None],
+            'tipoTecnologia': ['dog', 'elephant', 'tiger', None, 'lion', None, 'bc'],
+        }
+        df = pd.DataFrame(data)
+
+        df['tech'] = df.apply(lambda row: utils.concatenate_columns(row, 'Tecnologia', 'tipoTecnologia'), axis=1)
+
+        expected_df = pd.DataFrame(
+            data={
+                'Tecnologia': ['apple', 'banana', 'cherry', None, '123', 'ab', None],
+                'tipoTecnologia': ['dog', 'elephant', 'tiger', None, 'lion', None, 'bc'],
+                'tech': ['apple_dog', 'banana_elephant', 'cherry_tiger', None, '123_lion', 'ab', 'bc']
+            }
+        )
+        # print(f"df = {df}")
+        # print(f"expected_df = {expected_df}")
+
+        self.assertTrue(df.equals(expected_df))
 
 if __name__ == "__main__":
     unittest.main()
