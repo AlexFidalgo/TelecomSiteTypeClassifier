bcf34703027364728e2f5403a7c8d63d0db85a18
Alex <alexfidalgo10@gmail.com>
2023-12-03 16:20:33 -0300

    decision tree steps

--- ML_algorithms/DecisionTree/decision_tree.py
+++ ML_algorithms/DecisionTree/decision_tree.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 from clean_data_for_decision_tree import *
 
 # Find anatel file path
@@ -23,3 +26,24 @@ anatel = pd.get_dummies(anatel, columns=['BasicFeatures'], prefix='BasicFeatures
 # Station as the index
 df['Station'] = df['Station'].astype(int)
 anatel.set_index('Station', inplace=True)
+
+# Split data into features and target
+X = anatel.drop("SiteType", axis=1)
+y = anatel["SiteType"]
+
+# Split Data into Training and Testing Sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Build and Train the Decision Tree Model
+tree_model = DecisionTreeClassifier(random_state=42)
+tree_model.fit(X_train, y_train)
+
+# Use the trained model to make predictions on the test set
+y_pred = tree_model.predict(X_test)
+
+# Evaluate performance
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
+
+
+
